8b84c7fc064391152a47e1a5ed38e1a4cf51e3d7
jeongabae <gabea52@gmail.com>
2020-05-10 18:15:21 +0900

    [create] project init

---
+++ src/main/java/com/study/backjun/basic/backjun1000.java
@@ -0,0 +1,13 @@
+package com.study.backjun.basic;
+
+import java.util.Scanner;
+
+public class backjun1000 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
