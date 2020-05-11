49dd5da6a72daff9712562f38b967eb11d4c7335
jeongabae <gabea52@gmail.com>
2020-05-11 22:35:03 +0900

    [create] 백준1110번

---
+++ src/main/java/com/study/backjun/basic/backjun1110.java
@@ -0,0 +1,13 @@
+package com.study.backjun.basic;
+
+import java.util.Scanner;
+
+public class backjun1110 {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        double a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a/b);
+    }
+}
