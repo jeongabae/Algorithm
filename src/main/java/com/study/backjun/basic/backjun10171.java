d0df4e8bc894b86fd0c3cb4f6201780d6bff0018
jeongabae <gabea52@gmail.com>
2020-05-11 22:44:32 +0900

    [create] 백준 10171번

---
+++ src/main/java/com/study/backjun/basic/backjun10171.java
@@ -0,0 +1,10 @@
+package com.study.backjun.basic;
+
+public class backjun10171 {
+    public static void main(String[]args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
