fc8d6319996345906b17fb7b9039e4909e489139
jeongabae <gabea52@gmail.com>
2020-05-15 23:44:21 +0900

    [create] CharToCode

---
+++ src/main/java/com/study/javaex/CharToCode.java
@@ -0,0 +1,14 @@
+package com.study.javaex;
+
+public class CharToCode {
+    public static void main(String [] args) {
+        char ch = 'A';
+        int code = (int)ch; // ch애 저장된 값을 int타입으로 변환하여 저장한다.
+
+        System.out.printf("%c=%d(%xX)%n",ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c=%d(%xX)%n", hch, (int)hch, (int)hch);
+
+    }
+}
