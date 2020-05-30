bd920a2a1f1ac8e9a8c6cb74ed3a47addbbe4baf
jeongabae <gabea52@gmail.com>
2020-05-30 20:48:59 +0900

    [create] backjun10817

---
+++ src/main/java/com/study/backjun/basic/backjun10817.java
@@ -0,0 +1,24 @@
+package com.study.backjun.basic;
+//세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오.
+
+import java.util.Scanner;
+
+public class backjun10817 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        if((B<=A&&A<=C)||(C<=A&&A<=B)) {  //A가 중간값일때
+            System.out.println(A);
+        }
+        else if((A<=B&&B<=C)||(C<=B&&B<=A)) {//B가 중간값일때
+            System.out.println(B);
+        }
+            else{                           //C가 중간값일때
+                System.out.println(C);
+            }
+        }
+}
