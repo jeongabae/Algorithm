2a701ff2b2370e5be706bc91feb2d6bcc15e9406
jeongabae <gabea52@gmail.com>
2020-07-10 23:58:46 +0900

    [create] javaex

---
+++ src/main/java/com/study/javaex/ArrayEx4.java
@@ -0,0 +1,10 @@
+package com.study.javaex;
+
+public class ArrayEx4 {
+    public static void main(String[]args){
+        char[] abc = {'A', 'B', 'C', 'D'};
+        char[] num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        System.out.println(abc);
+        System.out.println(num);
+    }
+}
