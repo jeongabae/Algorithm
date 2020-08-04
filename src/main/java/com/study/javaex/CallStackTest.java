6eb90e1adb7357af5fb7490f5938c2b29ea1aa37
jeongabae <gabea52@gmail.com>
2020-08-04 23:42:00 +0900

    [create] CallStackTest

---
+++ src/main/java/com/study/javaex/CallStackTest.java
@@ -0,0 +1,15 @@
+package com.study.javaex;
+
+public class CallStackTest {
+    public static void main(String[]args){
+        firstMethod();
+    }
+
+    static void firstMethod(){
+        secondMethod();
+    }
+
+    static void secondMethod(){
+        System.out.println();
+    }
+}
