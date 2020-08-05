7d906ee9e3ebe4dbbccb126d5d6857d183d58475
jeongabae <gabea52@gmail.com>
2020-08-05 22:31:58 +0900

    [create] ArrayEx15

---
+++ src/main/java/com/study/javaex/ArrayEx15.java
@@ -0,0 +1,18 @@
+package com.study.javaex;
+
+public class ArrayEx15 {
+    public static void main(String[]args){
+        String source ="SOSHELP";
+        String[] morse = { ".-", "-. . .", "-.-.", "-..", "."
+        , "..-."};
+
+        String result="";
+
+        for(int i=0;i <source.length();i++){
+            result += morse[source.charAt(i)-'A'];
+        }
+
+        System.out.println("source:"+source);
+        System.out.println("morese:"+result);
+    }
+}
