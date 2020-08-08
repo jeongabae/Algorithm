cac94ddb48243fc54d2499811d367caf968695ed
jeongabae <gabea52@gmail.com>
2020-08-08 23:07:11 +0900

    [create] ThreadWaitEx1

---
+++ src/main/java/com/study/javaex/ThreadWaitEx1.java
@@ -0,0 +1,88 @@
+package com.study.javaex;
+
+import java.util.ArrayList;
+
+public class ThreadWaitEx1 {
+    public static void main(String []args) throws Exception{
+        Table table = new Table();
+
+        new Thread(new Cook(table), "COOK1").start();
+        new Thread(new Customer(table, "donut"), "CUST1").start();
+        new Thread(new Customer(table, "donut"), "CUST2").start();
+
+        Thread.sleep(1000);
+        System.exit(0);
+    }
+}
+
+class Customer implements Runnable{
+    private Table table;
+    private String food;
+
+    Customer(Table table, String food){
+        this.table = table;
+        this.food = food;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String name = Thread.currentThread().getName();
+
+            if(eatFood())
+                System.out.println(name + " ate a "+ food);
+            else
+                System.out.println(name+" failed to eat. :(");
+        }
+    }
+    boolean eatFood(){return table.remove(food);}
+}
+class Cook implements Runnable {
+    private Table table;
+
+    Cook(Table table) {
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int idx = (int) (Math.random() * table.dishNum());
+            table.add(table.dishNames[idx]);
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class Table{
+    String[] dishNames = {"donut", "donut", "burger"};
+    final int MAX_FOOD = 6;
+
+    private ArrayList<String> dishes = new ArrayList<>();
+
+    public void add(String dish){
+        if(dishes.size() >= MAX_FOOD)
+            return;
+        dishes.add(dish);
+        System.out.println("Dishes : "+dishes.toString());
+    }
+    public boolean remove(String dishName){
+        for(int i=0;i<dishes.size();i++) {
+            if (dishName.equals(dishes.get(i))) {
+                dishes.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    public int dishNum() {return dishNames.length;}
+}
