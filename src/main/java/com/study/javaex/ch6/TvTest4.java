397137ff7c4a7399ef206301f52f884c9f6b1ebb
jeongabae <gabea52@gmail.com>
2020-08-12 23:14:43 +0900

    [create] TvTest4

--- src/main/java/com/study/javaex/ch6/TvTest4.java
+++ src/main/java/com/study/javaex/ch6/TvTest4.java
@@ -9,15 +9,25 @@ class TvTest4{
             tvArr[i] = new Tv4();
             tvArr[i].channel = i+10;
         }
+        for(int i=0;i <tvArr.length;i++){
+            tvArr[i].channelUp();
+            System.out.printf("tvArr[%d].channel=%d%n",i,tvArr[i].channel);
+        }
 
     }
         }
-class Tv4{
-    public static void main(String args[]){
-        Tv t;
-        t = new Tv();
-        t.channel = 7;
-        t.channelDown();
-        System.out.println("현재 체널은 "+t.channel+" 입니다.");
+class Tv4 {
+    String color;
+    boolean power;
+    int channel;
+
+    void power(){
+        power = !power;
+    }
+    void channelUp(){
+        ++channel;
+    }
+    void channelDown(){
+        --channel;
     }
 }
