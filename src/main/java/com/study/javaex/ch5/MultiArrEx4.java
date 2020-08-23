27d83064f1f7d541b0467dcdd8cce6023472dda6
jeongabae <gabea52@gmail.com>
2020-08-23 23:48:55 +0900

    [create] MultiArrEx4

---
+++ src/main/java/com/study/javaex/ch5/MultiArrEx4.java
@@ -0,0 +1,25 @@
+package com.study.javaex.ch5;
+
+import java.util.*;
+public class MultiArrEx4 {
+    public static void main(String[] args){
+        String[][] words = {
+                {"chair","의자"},
+                {"computer","컴퓨터"},
+                {"integer","정수"}
+        };
+        Scanner scanner1 = new Scanner(System.in);
+
+        for(int i=0;i<words.length;i++){
+            System.out.printf("Q%d. %s의 뜻은?",i+1, words[i][0]);
+
+            String tmp = scanner1.nextLine();
+
+            if(tmp.equals(words[i][1])){
+                System.out.printf("정답입니다.%n%n");
+            } else{
+                System.out.printf("틀렸습니다. 정답은 %s입니다. %n%n", words[i][1]);
+            }
+        }
+    }
+}
