74ddbf78d0d37a2180849074cea5326980c27253
jeongabae <gabea52@gmail.com>
2020-08-25 23:38:52 +0900

    [create] FileViewer

---
+++ src/main/java/com/study/javaex/ch15/FileViewer.java
@@ -0,0 +1,13 @@
+package com.study.javaex.ch15;
+import java.io.*;
+public class FileViewer {
+public static void main(String[]args) throws IOException {
+    FileInputStream fis = new FileInputStream(args[0]);
+    int data = 0;
+
+    while((data = fis.read()) != -1){
+        char c= (char)data;
+        System.out.print(c);
+    }
+}
+}
