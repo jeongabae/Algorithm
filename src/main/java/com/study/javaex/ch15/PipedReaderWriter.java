ca9e7a256769a97437fc89d9a7a89fb2d0905b98
jeongabae <gabea52@gmail.com>
2020-08-26 22:58:38 +0900

    [create] PipedReaderWriter

---
+++ src/main/java/com/study/javaex/ch15/PipedReaderWriter.java
@@ -0,0 +1,76 @@
+package com.study.javaex.ch15;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.io.*;
+public class PipedReaderWriter {
+    public static void main(String[]args){
+        InputThread inThread = new InputThread("InputThread");
+        OutputThread outThread = new OutputThread("OutputThread");
+
+        inThread.connnect(outThread.getOutput());
+
+        inThread.start();
+        outThread.start();
+    }
+}
+
+class InputThread extends Thread {
+    PipedReader input = new PipedReader();
+    StringWriter sw = new StringWriter();
+
+    InputThread(String name) {
+        super(name);
+    }
+
+    public void run() {
+        try {
+            int data = 0;
+
+            while ((data = input.read()) != -1) {
+                sw.write(data);
+            }
+            System.out.println(getName() + "received: " + sw.toString());
+        } catch (IOException e) {
+        }
+    }
+    public PipedReader getInput(){
+        return input;
+    }
+    public void connect(PipedWriter output){
+        try{
+            input.connect(output);
+        } catch (IOException e) {
+        }
+    }
+}
+
+class OutputThread extends Thread{
+    PipedWriter output = new PipedWriter();
+
+    OutputThread(String name){
+        super(name);
+    }
+
+    public void run(){
+        try{
+            String msg = "Hello";
+            System.out.println(getName()+"sent : "+msg);
+            output.write(msg);
+            output.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    public PipedWriter getOutput(){
+        return output;
+    }
+
+    public void connect(PipedWriter input){
+        try{
+            output.connect(input);
+        } catch (IOException e) {
+        }
+    }
+}
