7ddfb8d043591fe3e8414a5fba760d6552e8391e
jeongabae <gabea52@gmail.com>
2020-08-31 23:13:59 +0900

    [revise] BufferedReaderEx1

--- src/main/java/com/study/javaex/ch15/BufferedReaderEx1.java
+++ src/main/java/com/study/javaex/ch15/BufferedReaderEx1.java
@@ -4,15 +4,19 @@ import java.io.*;
 public class BufferedReaderEx1 {
     public static void main(String[]args){
         try{
-            FileReader fr = new FileReader("BufferedReaderEx1.java");
+            FileReader fr = new FileReader("/Users/jeongabae/IdeaProjects/gabae1/src/main/java/com/study/javaex/ch15/BufferedReaderEx1.java");
             BufferedReader br  = new BufferedReader(fr);
 
             String line = "";
+            System.out.println(br.toString());
             for(int i=1;(line = br.readLine())!=null;i++){
+                System.out.println(line);
                 if(line.indexOf(";")!= -1)
                     System.out.println(i+":"+line);
             }
             br.close();
-        } catch (IOException e) {}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
