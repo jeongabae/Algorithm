fbb1c2f62616c175e380acf9c75de0ac83352aca
jeongabae <gabea52@gmail.com>
2020-08-31 23:12:19 +0900

    [revise] FileEx3

--- src/main/java/com/study/javaex/ch15/FileEx3.java
+++ src/main/java/com/study/javaex/ch15/FileEx3.java
@@ -15,7 +15,44 @@ public class FileEx3 {
         File dir = new File(args[0]);
 
         if(!dir.exists() || !dir.isDirectory()){
-
+            System.out.println("유효하지 않은 디렉토리입니다.");
+            System.exit(0);
         }
+
+        printFileList(dir);
+
+        System.out.println("총 "+totalFiles + "개의 파일");
+        System.out.println("총 "+totalDirs + "개의 디렉토리");
+    }
+
+    public static void printFileList(File dir){
+        System.out.println(dir.getAbsolutePath()+" 디렉토리");
+        File[] files = dir.listFiles();
+
+        ArrayList subDir = new ArrayList();
+
+        for(int i=0;i<files.length;i++){
+            String filename = files[i].getName();
+
+            if(files[i].isDirectory()){
+                filename = "[" + filename + "]";
+                subDir.add(i+"");
+            }
+            System.out.println(filename);
+        }
+        int dirNum = subDir.size();
+        int fileNum = files.length - dirNum;
+
+        totalFiles += fileNum;
+        totalDirs += dirNum;
+
+        System.out.println(fileNum + "개의 파일, "+dirNum+"개의 디렉토리");
+        System.out.println();
+
+        for(int i=0;i < subDir.size();i++){
+            int index = Integer.parseInt((String)subDir.get(i));
+            printFileList(files[index]);
+        }
+
     }
 }
