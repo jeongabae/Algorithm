66f593a4cacdbfac2c08e111960f67c69bfe3f0c
jeongabae <gabea52@gmail.com>
2020-09-02 23:10:08 +0900

    [create] SerialEx2

---
+++ src/main/java/com/study/javaex/ch15/SerialEx2.java
@@ -0,0 +1,12 @@
+package com.study.javaex.ch15;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SerialEx2 {
+    public static void main(String[]args){
+        try{
+            String fileName = "UserInfo.ser";
+        }
+    }
+}
