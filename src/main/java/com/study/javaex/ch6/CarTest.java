95181ab09d5e7c97dd3cac52b57c9785e5856c2f
jeongabae <gabea52@gmail.com>
2020-09-10 23:37:58 +0900

    [modified ]

--- src/main/java/com/study/javaex/ch6/CarTest.java
+++ src/main/java/com/study/javaex/ch6/CarTest.java
@@ -15,12 +15,12 @@ class Car{
 }
 class CarTest {
     public static void main(String[]args){
-        Car c1 = new Car();
+        Car1 c1 = new Car1();
         c1.color = "white";
         c1.gearType = "auto";
         c1.door = 4;
 
-        Car c2 = new Car("white", "auto",4);
+        Car1 c2 = new Car1("white", "auto",4);
 
         System.out.println("c1의 color="+c1.color+",gearType="+c1.gearType+",door="+c1.door);
         System.out.println("c2의 color="+c2.color+",gearType="+c2.gearType+",door="+c2.door);
