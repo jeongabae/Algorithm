02b31e6bf04dfd07bfffb464eb550af0fefc86b1
jeongabae <gabea52@gmail.com>
2020-12-24 22:27:58 +0900

    [create] Bj2741

---
+++ src/main/java/com/study/baekjun/for_3/Bj2741.java
@@ -0,0 +1,13 @@
+package com.study.baekjun.for_3;
+
+import java.util.Scanner;
+
+public class Bj2741 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=1;i<=N;i++)
+            System.out.println(i);
+    }
+}
