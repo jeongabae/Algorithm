e5cdcd5fce5e665a3c535b9bf28082354303402a
jeongabae <gabea52@gmail.com>
2021-01-04 20:35:31 +0900

    [create] Bj2558,11942

---
+++ src/main/java/com/study/baekjun/implementation/Bj11942.java
@@ -0,0 +1,7 @@
+package com.study.baekjun.implementation;
+
+public class Bj11942 {
+    public static void main(String[]args){
+        System.out.println("고려대학교");
+    }
+}
---
+++ src/main/java/com/study/baekjun/implementation/Bj2558.java
@@ -0,0 +1,15 @@
+package com.study.baekjun.implementation;
+
+import java.io.*;
+
+public class Bj2558 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+       System.out.println(A+B);
+    }
+}
+
+
