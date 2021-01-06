1c06ba1391cf24bed0dd7d8f5d2b12614f8002c2
jeongabae <gabea52@gmail.com>
2021-01-06 23:28:03 +0900

    [create] Bj11719

---
+++ src/main/java/com/study/baekjun/implementation/Bj11719.java
@@ -0,0 +1,13 @@
+package com.study.baekjun.implementation;
+
+import java.io.*;
+
+public class Bj11719 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        for(int i=1;(line=br.readLine())!=null;i++){
+        System.out.println(line);
+        }
+    }
+}
