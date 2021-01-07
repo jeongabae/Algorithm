720767ec1406a3fd305058ab4909fa6199a4ef67
jeongabae <gabea52@gmail.com>
2021-01-07 22:21:02 +0900

    [create] Bj10757 큰 수 A+B

---
+++ src/main/java/com/study/baekjun/math_8/Bj10757.java
@@ -0,0 +1,15 @@
+package com.study.baekjun.math_8;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Bj10757 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        BigInteger A=sc.nextBigInteger();
+        BigInteger B=sc.nextBigInteger();
+        System.out.println(A.add(B));
+
+
+    }
+}
